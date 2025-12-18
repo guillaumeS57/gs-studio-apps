9b5fa0244b1f20a608f7c3c56cee802269e02dcd
guillaumeS57 <guillaume.schoirfer@gmail.com>
2025-12-18 11:17:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'gs-studio-v1';
+const ASSETS = [
+  'index.html',
+  'Tapple.html',
+  'GenerateurPW.html',
+  'style.css',
+  'manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Intercepter les requêtes pour servir le cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
